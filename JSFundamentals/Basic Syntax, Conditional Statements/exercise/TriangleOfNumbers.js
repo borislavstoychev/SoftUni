022caa731c74184ec24efe56c0e0d078df27784f
borislavstoychev <stoy4ew@gmail.com>
2021-04-20 12:25:54 +0300

    second solution

--- JSFundamentals/Basic Syntax, Conditional Statements/exercise/TriangleOfNumbers.js
+++ JSFundamentals/Basic Syntax, Conditional Statements/exercise/TriangleOfNumbers.js
@@ -1,11 +1,25 @@
-function solve(num) {
-    for (let i = 1; i <= num; i++) {
-        result = ""
-        for (let j = 1; j <= i; j++) {
-            result += i + " " 
+// function solve(num) {
+//     for (let i = 1; i <= num; i++) {
+//         result = ""
+//         for (let j = 1; j <= i; j++) {
+//             result += i + " " 
+//         }
+//         console.log(result)
+//     }
+// }
+
+function solve(n) {
+    function displayItem(num) {
+        let sequence = []
+        for (let i = 1; i <= num; i++) {
+            sequence.push(num)
         }
-        console.log(result)
+        return sequence.join(" ")
+   
+    } 
+    for (let j = 1; j <= n; j++) {
+        console.log(displayItem(j))
     }
 }
 
-solve(100)
+solve(5)
