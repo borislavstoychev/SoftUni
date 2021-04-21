16e2705d203f69d9cd4259b08194af45c5a9d85c
borislavstoychev <stoy4ew@gmail.com>
2021-04-21 21:05:29 +0300

    06. Fuel Money

--- JSFundamentals/Data Types and Variables/lab/FuelMoney.js
+++ JSFundamentals/Data Types and Variables/lab/FuelMoney.js
@@ -2,7 +2,7 @@ function trip(distance, passeengers, price) {
     let neededFule = (distance / 100) * 7;
     neededFule += passeengers * 0.1;
     let money = neededFule * price
-    console.log(`Needed money for that trip is ${money} lv.`)
+    console.log(`Needed money for that trip is ${money}lv.`)
 }
 
 trip(260, 9, 2.49)
