5075b05b3f2c85d775a01afc42e10bef0e32e143
borislavstoychev <stoy4ew@gmail.com>
2021-04-23 12:20:52 +0300

    07. Lower or Upper

--- JSFundamentals/Data Types and Variables/exercise/LowerOrUpper.js
+++ JSFundamentals/Data Types and Variables/exercise/LowerOrUpper.js
@@ -1,10 +1,6 @@
 function lowerOrUpper(char) {
-    if (char === char.toUpperCase()) {
-        console.log('upper-case')
-    } else {
-        console.log('lower-case')
-    }
-
+    console.log(char === char.toUpperCase() ? 'upper-case' : 'lower-case')
 }
+
 lowerOrUpper('L')
 lowerOrUpper('i')
