595383897a6a605218deb11cb0129e0445781822
borislavstoychev <stoy4ew@gmail.com>
2021-04-23 15:14:07 +0300

    02. Day of Week

--- JSFundamentals/Arrays/lab/DayOfWeek.js
+++ JSFundamentals/Arrays/lab/DayOfWeek.js
@@ -1,14 +1,14 @@
 function dayOfWeek(num) {
     const days = {
         1: "Monday",
-        2: "Thuesday",
-        3: "Wensday",
+        2: "Tuesday",
+        3: "Wednesday",
         4: "Thursday",
         5: "Friday",
         6: "Saturday",
         7: "Sunday"
     }
-    console.log(num <= 7 ? days[num] : "Invwlid day!")
+    console.log(num <= 7 ? days[num] : "Invalid day!")
 }
 
 dayOfWeek(3)
