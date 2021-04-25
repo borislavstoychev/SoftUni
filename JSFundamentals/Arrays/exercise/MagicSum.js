799ae3061c7787f0d6e9c7ff210d70a75372f450
borislavstoychev <stoy4ew@gmail.com>
2021-04-25 14:12:17 +0300

    Magicsum

--- JSFundamentals/Arrays/exercise/MagicSum.js
+++ JSFundamentals/Arrays/exercise/MagicSum.js
@@ -2,7 +2,7 @@ function magicSum(arr, num) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i +1; j < arr.length; j++) {
             if (arr[i] + arr[j] === num) {
-                console.log(`${arr[i]} ${arr[j]}`);
+                console.log(`${arr[i]} ${arr[j]}`)
             }
         }
     }   
