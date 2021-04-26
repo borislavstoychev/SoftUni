d3ffd6f7f2bcfff342cfcd374c095d047da359b2
borislavstoychev <stoy4ew@gmail.com>
2021-04-26 21:19:40 +0300

    05. Simple Calculator

---
+++ JSFundamentals/Functions/lab/SimpleCalculator.js
@@ -0,0 +1,19 @@
+function simpleCalculator(numOne, numTwo, operator) {
+    
+    const calculator = {
+        "multiply": numOne * numTwo,
+        "divide": numOne / numTwo,
+        "add": numOne + numTwo,
+        "subtract": numOne - numTwo
+    }
+    return calculator[operator]
+
+   
+}
+const print = function(n, n1, op) {
+    console.log(simpleCalculator(n, n1, op));
+}
+print(5, 5, 'multiply')
+print(5, 5, 'divide')
+print(5, 5, 'add')
+print(5, 5, 'subtract')
