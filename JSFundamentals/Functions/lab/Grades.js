1936f570cdce32f7e60b55105d60b3c8696bf5d5
borislavstoychev <stoy4ew@gmail.com>
2021-04-26 19:46:27 +0300

    02. Grades

---
+++ JSFundamentals/Functions/lab/Grades.js
@@ -0,0 +1,18 @@
+function grade(num) {
+    switch (true) {
+        case (num < 3):
+            return `Fail (${num.toFixed(2)})`;
+        case  (num < 3.50):
+            return `Poor (${num.toFixed(2)})`;
+        case  (num < 4.50):
+            return `Good (${num.toFixed(2)})`;
+        case ( num < 5.50):
+            return `Very good (${num.toFixed(2)})`;
+        case  (num <= 6):
+            return `Excellent (${num.toFixed(2)})`;
+    }
+}
+
+console.log(grade(3.33))
+console.log(grade(4.50))
+console.log(grade(2.90))
