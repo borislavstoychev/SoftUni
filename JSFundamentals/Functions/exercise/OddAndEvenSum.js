19419cadd3e763fc15cc64ea982810f67b411845
borislavstoychev <stoy4ew@gmail.com>
2021-05-04 23:08:43 +0300

    04. Odd And Even Sum

---
+++ JSFundamentals/Functions/exercise/OddAndEvenSum.js
@@ -0,0 +1,11 @@
+function foo(num) {
+    let evenSum = 0
+    let oddSum = 0
+    for (const n of String(num)) {
+        Number(n) % 2 == 0 ? evenSum += Number(n) : oddSum += Number(n)
+    }
+    return ` Odd sum = ${oddSum}, Even sum = ${evenSum}`
+}
+
+console.log(foo(1000435))
+console.log(foo(3495892137259234))
