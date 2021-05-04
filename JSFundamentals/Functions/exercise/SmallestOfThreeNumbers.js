41a4b50e1df176db527c9c7f488626a88aaf4b4e
borislavstoychev <stoy4ew@gmail.com>
2021-05-04 22:22:21 +0300

    01. Smallest of Three Numbers

---
+++ JSFundamentals/Functions/exercise/SmallestOfThreeNumbers.js
@@ -0,0 +1,7 @@
+function smalestNumber(...args) {
+    return Math.min(...args);
+}
+
+console.log(smalestNumber(2,5,3, 5, -5 ))
+console.log(smalestNumber(600,342,123))
+console.log(smalestNumber(25,21,4))
