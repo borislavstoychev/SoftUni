18ffcb0714320520b0561ef9352ba70584dc5760
borislavstoychev <stoy4ew@gmail.com>
2021-05-04 23:30:16 +0300

    05. Palindrome Integers

---
+++ JSFundamentals/Functions/exercise/PalindromeIntegers.js
@@ -0,0 +1,15 @@
+function palindrome(arr) {
+    for (const num of arr) {
+        let palindromNum = Number(num
+        .toString()
+        .split("")
+        .reverse()
+        .join(""))
+        num === palindromNum
+        ? console.log("true") 
+        : console.log ("false")
+    }
+}
+
+palindrome([123,323,421,121])
+palindrome([32,2,232,1010])
