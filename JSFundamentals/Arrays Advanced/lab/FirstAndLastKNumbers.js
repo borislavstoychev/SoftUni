256b2ac24ed1b074776fda37dfba4e37505ec06c
borislavstoychev <stoy4ew@gmail.com>
2021-05-08 13:53:12 +0300

    03. First and Last K Numbers

--- JSFundamentals/Arrays Advanced/lab/FirstAndLastKNumbers.js
+++ JSFundamentals/Arrays Advanced/lab/FirstAndLastKNumbers.js
@@ -1,11 +1,11 @@
 function firstLast(arr) {
-    let index = arr.shift()
-    let first = arr.slice(0, index)
-    let second = arr.slice(- index)
-    return first.join(" ") + "\n" + second.join(" ")
+    let index = arr.shift();
+    let first = arr.slice(0, index);
+    let second = arr.slice(- index);
+    return first.join(" ") + "\n" + second.join(" ");
 }
 
 console.log(firstLast([2, 
-    7, 8, 9]))
+    7, 8, 9]));
 console.log(firstLast([3,
-    6, 7, 8, 9]))
+    6, 7, 8, 9]));
