415a5af3eaea16dfcc7cd981709aa8654f1d2423
borislavstoychev <stoy4ew@gmail.com>
2021-05-14 14:19:04 +0300

    07. Class Vehicle

---
+++ JSFundamentals/Objects and Classes/exercise/ClassVehicle.js
@@ -0,0 +1,19 @@
+function classVehicle(arr) {
+    class Vehicle {
+        constructor(type, model, parts, fuel) {
+            this.type = type,
+            this.model = model,
+            this.parts = {... parts, quality: parts.power * parts.engine},
+            this.fuel = fuel;
+        }
+        drive(x) {
+            this.fuel -= x;
+        }
+    }
+    let parts = { engine: 6, power: 100 };
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100);
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality);
+}
+classVehicle()
