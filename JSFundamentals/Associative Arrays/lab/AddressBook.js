37e8e5b8433096bcc8cc4ff7e61509314f935793
borislavstoychev <stoy4ew@gmail.com>
2021-05-17 14:22:44 +0300

    03. Address Book

--- JSFundamentals/Associative Arrays/lab/AddressBook.js
+++ JSFundamentals/Associative Arrays/lab/AddressBook.js
@@ -2,11 +2,11 @@ function addressBook(arr) {
     const book = {};
     arr.forEach(element => {
        let [name,addres] = element.split(":");
-       book[name] = addres
+       book[name] = addres;
     });
     let sorted = Object.entries(book).sort((a, b) => a[0].localeCompare(b[0]));
     sorted.forEach(element => {
-        let [key,value] = element
+        let [key,value] = element;
         console.log(`${key} -> ${value}`);
     });
 }
