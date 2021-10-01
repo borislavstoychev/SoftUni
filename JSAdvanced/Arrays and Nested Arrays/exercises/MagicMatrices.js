391a37faa63abc3362ada43e22d69b124c6ac293
borislavstoychev <stoy4ew@gmail.com>
2021-10-01 11:57:21 +0300

    fixed megic matrices error

--- JSAdvanced/Arrays and Nested Arrays/exercises/MagicMatrices.js
+++ JSAdvanced/Arrays and Nested Arrays/exercises/MagicMatrices.js
@@ -6,7 +6,7 @@ function matrix(arr){
     
     for (let i= 0; i < arr.length; i++){
         let sum = 0;
-        for (let j=0; j < arr[i].length; j++){
+        for (let j=0; j < arr.length; j++){
             sum += arr[i][j]
         }
         result.push(sum)
