9b04ff0b312ce32c2ed789f937d1a42c68d3b898
borislavstoychev <stoy4ew@gmail.com>
2021-11-05 12:13:44 +0200

    Accordion

--- JSAdvanced/DOM Introduction/exercise/03. Accordion/accordion.js
+++ JSAdvanced/DOM Introduction/exercise/03. Accordion/accordion.js
@@ -1,3 +1,7 @@
 function toggle() {
-    console.log('TODO:...');
+    let text = document.getElementById('extra');
+    text.style.display == 'block' ? text.style.display = 'none': text.style.display = 'block'
+
+    const btn = document.querySelector('.button')
+    btn.textContent == 'Less' ? btn.textContent = 'More': btn.textContent = 'Less'
 }
