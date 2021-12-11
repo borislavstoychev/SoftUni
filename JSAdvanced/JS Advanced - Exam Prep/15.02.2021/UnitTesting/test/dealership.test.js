5852b535483d347115a606416612acebe2316ef6
borislavstoychev <stoy4ew@gmail.com>
2021-12-11 16:29:50 +0200

    Unit Testing

---
+++ JSAdvanced/JS Advanced - Exam Prep/15.02.2021/UnitTesting/test/dealership.test.js
@@ -0,0 +1,43 @@
+let dealership = require('../dealership');
+let {assert, expect} = require('chai');
+
+describe("dealership", function() {
+    describe("test newCarCost", function() {
+
+        it("should return final price, recived discoun from old car ", function() {
+            let oldCarModel = 'Audi A4 B8'
+            let  newCarPrice = 25000
+            expect(dealership.newCarCost(oldCarModel, newCarPrice)).to.equal(10000)
+        });
+        it('should return new car price, without discount', function(){
+            let oldCarModel = 'Audi A3 B7'
+            let  newCarPrice = 25000
+            expect(dealership.newCarCost(oldCarModel, newCarPrice)).to.equal(25000)
+        })
+     });
+
+     describe("test carEquipment", function() {
+
+        it("should return list with selected extras", function() {
+            let extrasArr = ['heated seats', 'sliding roof', 'sport rims', 'navigation'];
+            let  indexArr = [0, 2, 3]
+            console.log(dealership.carEquipment(extrasArr,  indexArr))
+            expect(dealership.carEquipment(extrasArr,  indexArr)).to.eql(['heated seats', 'sport rims', 'navigation'])
+        });
+     });
+
+     describe("test euroCategory", function() {
+
+        it("should return 5% discount the category is bigger or equal to 4", function() {
+            let expectedResuld = `We have added 5% discount to the final price: 14250.`
+            expect(dealership.euroCategory(4)).to.equal(expectedResuld)
+        });
+        it('should return expected message for less then 4 category', function(){
+            let expectedResuld = 'Your euro category is low, so there is no discount from the final price!'
+            expect(dealership.euroCategory(3)).to.equal(expectedResuld)
+        })
+     });
+
+
+
+});
