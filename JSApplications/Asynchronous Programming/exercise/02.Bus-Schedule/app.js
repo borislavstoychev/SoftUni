34068f6eadd497806c3aa365427782aed733e11b
borislavstoychev <stoy4ew@gmail.com>
2022-01-11 22:18:01 +0200

    Bus Schedule

--- JSApplications/Asynchronous Programming/exercise/02.Bus-Schedule/app.js
+++ JSApplications/Asynchronous Programming/exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,36 @@
 function solve() {
+    let nextStopId = 'depot'
+    const divInfo = document.getElementById('info');
+    const stopInfoSpan = document.querySelector('#info .info')
+    const buttonDepart = document.getElementById('depart');
+    const buttonArrive = document.getElementById('arrive');
+
 
     function depart() {
-        console.log('Depart TODO...');
+        if (stopInfoSpan.getAttribute('next-stop-id') !== null){
+            nextStopId = stopInfoSpan.getAttribute('next-stop-id')
+        };
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+        .then(response => response.json())
+        .then(data => {
+            stopInfoSpan.setAttribute('data-stop-name', data.name);
+            stopInfoSpan.setAttribute('next-stop-id', data.next)
+            buttonDepart.disabled = true;
+            buttonArrive.disabled = false;
+            divInfo.textContent = `Next stop ${data.name}`
+        })
+        .catch(error =>{
+            divInfo.textContent = "Error";
+            buttonDepart.disabled = true;
+            buttonArrive.disabled = true;
+        })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        let name = stopInfoSpan.getAttribute('data-stop-name')
+        divInfo.textContent = `Arriving ${name}`;
+        buttonDepart.disabled = false;
+        buttonArrive.disabled = true;
     }
 
     return {
