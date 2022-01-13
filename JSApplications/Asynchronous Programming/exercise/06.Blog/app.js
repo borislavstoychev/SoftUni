3f22e3c16cafc0654b45dee266c68593b91438a9
borislavstoychev <stoy4ew@gmail.com>
2022-01-13 16:30:46 +0200

    Blog

--- JSApplications/Asynchronous Programming/exercise/06.Blog/app.js
+++ JSApplications/Asynchronous Programming/exercise/06.Blog/app.js
@@ -1,5 +1,48 @@
 function attachEvents() {
-    console.log('TODO...');
+    const btnLoad = document.getElementById('btnLoadPosts');
+    const btnView = document.getElementById('btnViewPost');
+    const sectionPost = document.getElementById('posts');
+    const h1Title = document.getElementById('post-title');
+    const pBody = document.getElementById('post-body');
+    const ulComent= document.getElementById('post-comments');
+
+    btnLoad.addEventListener('click', fetchData)
+
+    function fetchData(){
+        fetch(`http://localhost:3030/jsonstore/blog/posts`)
+        .then(response => response.json()
+        .then(posts => {
+            Object.entries(posts)
+            .forEach(post => {
+                const option = document.createElement("option");
+                option.setAttribute('value', post[0])
+                option.textContent = post[1].title;
+                sectionPost.appendChild(option)
+            })
+            btnView.addEventListener('click', fetchComment)
+
+            function fetchComment(){
+                ulComent.innerText = '';
+                fetch('http://localhost:3030/jsonstore/blog/comments')
+                .then(response => response.json())
+                .then(data => {
+                    let id = sectionPost.value;
+                    let comments = Object.values(data).filter(({postId}) => postId === id)
+                    h1Title.textContent = posts[id]['title'];
+                    pBody.textContent = posts[id]['body'];
+                    comments.forEach(comment => {
+                        const liComment = document.createElement('li');
+                        liComment.id = comment.id;
+                        liComment.textContent = comment.text;
+
+                        ulComent.appendChild(liComment)
+                    })
+
+                })
+            }
+        }))
+    }
+    
 }
 
 attachEvents();
